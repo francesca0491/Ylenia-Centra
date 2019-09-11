51b8f6aa76c67d9da7b18edcfb781a8c26c68f6b
francesca0491 <53080973+francesca0491@users.noreply.github.com>
2019-09-11 16:07:40 +0200

    Update thiscript.js

--- js/thiscript.js
+++ js/thiscript.js
@@ -15,20 +15,16 @@ var firebaseConfig = {
   const emailInput = document.getElementById("email");
   const phoneInput = document.getElementById("phone");
   const messageInput = document.getElementById("message");
-    
-    if (nameInput.value == "") {nameInput.value = "zzdefault";}
-    if (phoneInput.value == "") {phoneInput.value = "zzdefault";}
-    if (messageInput.value == "") {messageInput.value = "zzdefault";}
-    if (emailInput.value == "") {emailInput.value = "zzdefault@zzdefault";}
+
   var inputUser = {value: "zzdefault"};
   var tempo = new Date();
 
   sendMessageButton2.addEventListener("click", e => {
-       document.getElementById("modal-body").innerHTML =
-    '<div class="modal-title">\r'+
-    '<h2 class="text-center">Grazie per esserti registrato!</h2>\r'+
-    '</div>\r'
-    ;
+          
+    if (nameInput.value == "") {nameInput.value = "zzdefault";}
+    if (phoneInput.value == "") {phoneInput.value = "zzdefault";}
+    if (messageInput.value == "") {messageInput.value = "zzdefault";}
+    if (emailInput.value == "") {emailInput.value = "zzdefault@zzdefault";}
     inputUser.value = nameInput.value +"_"+ tempo.getMonth()+ tempo.getDate()+ tempo.getHours()+tempo.getMinutes()+tempo.getSeconds();  
     path = inputUser.value;
   	database.ref(path).set({
