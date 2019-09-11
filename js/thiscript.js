488447fd47b60611a6e57548c0413c0ec627cf14
francesca0491 <53080973+francesca0491@users.noreply.github.com>
2019-09-11 15:16:16 +0200

    Update thiscript.js

--- js/thiscript.js
+++ js/thiscript.js
@@ -15,11 +15,13 @@ var firebaseConfig = {
   const emailInput = document.getElementById("email");
   const phoneInput = document.getElementById("phone");
   const messageInput = document.getElementById("message");
-
+    
+    if (nameInput.value == "") {inputName.value = "zzdefault";}
+    if (phoneInput.value == "") {phoneInput.value = "zzdefault";}
+    if (messageInput.value == "") {messageInput.value = "zzdefault";}
+    if (emailInput.value == "") {inputEmail.value = "zzdefault@zzdefault";}
   var inputUser = {value: "zzdefault"};
   var tempo = new Date();
-  
-
 
   sendMessageButton2.addEventListener("click", e => {
     inputUser.value = nameInput.value +"_"+ tempo.getMonth()+ tempo.getDate()+ tempo.getHours()+tempo.getMinutes()+tempo.getSeconds();  
@@ -30,4 +32,9 @@ var firebaseConfig = {
   		phone: phoneInput.value,
   		message: messageInput.value,
   	});
+      document.getElementById("modal-body").innerHTML =
+    '<div class="modal-title">\r'+
+    '<h2 class="text-center">Grazie per esserti registrato!</h2>\r'+
+    '</div>\r'
+    ;
   });
